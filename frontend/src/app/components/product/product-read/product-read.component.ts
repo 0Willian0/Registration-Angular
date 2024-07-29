6454eda058e38aa5f23711cbe6206bc36259431b
0Willian0 <willian-watanabe@outlook.com>
2024-07-29 20:49:02 -0300

    Product Table

---
+++ frontend/src/app/components/product/product-read/product-read.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit} from '@angular/core';
+import { Product } from '../product.model';
+import { ProductService } from '../product.service';
+import { MatTableModule, MatTable } from '@angular/material/table';
+import { CurrencyPipe } from '@angular/common';
+import { LOCALE_ID } from '@angular/core';
+
+import localePt from '@angular/common/locales/pt'
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(localePt)
+
+@Component({
+  selector: 'app-product-read',
+  standalone: true,
+  imports: [MatTableModule,MatTable,CurrencyPipe],
+  providers:[{
+    provide:LOCALE_ID,
+    useValue: 'pt-BR'
+  }],
+  templateUrl: './product-read.component.html',
+  styleUrl: './product-read.component.css'
+})
+export class ProductReadComponent implements OnInit{
+
+    products: Product[] = []
+    displayedColumns = ['id', 'name', 'price']
+    constructor(private productSevice:ProductService){
+
+    }
+
+    ngOnInit():void{
+      this.productSevice.read().subscribe(products =>{
+        this.products = products
+        console.log(products)
+      })
+    }
+}
